6245d1e0f83833dd651dfdd891419eb5df173219
Replit user <>
2023-03-16 19:49:24 -0400

    Gpt 3.5

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,18 +1,18 @@
-export default async function (req, res) {
+import { Configuration, OpenAIApi } from "openai";
 
-  const response = await fetch(process.env.LCC_ENDPOINT_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Api-Key": process.env.LCC_TOKEN
-    },
-    body: JSON.stringify({
-      question: req.body.question,
-      history: req.body.history
-    }),
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export default async function(req, res) {
+  const completion = await openai.createChatCompletion({
+    // You need early access to GPT-4, otherwise use "gpt-3.5-turbo"
+    model: "gpt-3.5-turbo",
+    messages: [{ "role": "system", "content": "You are a helpful assistant." }].concat(req.body.messages)
+
   });
+  res.status(200).json({ result: completion.data.choices[0].message })
 
-    const data = await response.json();
-
-    res.status(200).json({ result: data })
 }
